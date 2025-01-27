ab3321966eec8a9f454fffd33036d2d836b01a58
Ayush Katare <katareayush2005@gmail.com>
2025-01-27 16:42:52 +0530

    trying cors fix/ayush

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'https://demo-test-94vj.vercel.app', 
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'], 
 };
